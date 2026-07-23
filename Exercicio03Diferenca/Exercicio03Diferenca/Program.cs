cb746d82d9719ade125be1740cbe18c5d3351850
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-23 04:53:16 -0300

    feat: solve exercise 03

---
+++ Exercicio03Diferenca/Exercicio03Diferenca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio03Diferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+
+            Console.Write("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            C = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de D: ");
+            D = int.Parse(Console.ReadLine());
+
+            int diferenca = (A * B) - (C * D);
+
+            Console.WriteLine("Diferença = " + diferenca);
+        }
+    }
+}
