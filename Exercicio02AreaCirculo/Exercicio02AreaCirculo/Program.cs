2af08f449393118a16f03facf49cf07e40a2c257
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-23 04:46:45 -0300

    feat: solve exercise 02

---
+++ Exercicio02AreaCirculo/Exercicio02AreaCirculo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area;
+            double pi = 3.14159;
+
+            Console.Write("Digite o valor do raio: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A área do círculo é: " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
