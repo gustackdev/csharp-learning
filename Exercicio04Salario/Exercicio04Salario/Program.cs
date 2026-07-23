8cf896b290eec345fa2a52d841bf262d351a9e58
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-23 05:02:22 -0300

    feat: solve exercise 04

---
+++ Exercicio04Salario/Exercicio04Salario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio04Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numfuncionario;
+            int horastrabalhadas;
+            int valorhora;
+            decimal salario;
+
+            Console.WriteLine("Digite o número do funcionário: ");
+            numfuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite as horas trabalhadas: ");
+            horastrabalhadas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da hora deste funcionário: ");
+            valorhora = int.Parse(Console.ReadLine());
+
+            salario = horastrabalhadas * valorhora;
+
+            Console.WriteLine("Salário: "+salario);
+        }
+    }
+}
