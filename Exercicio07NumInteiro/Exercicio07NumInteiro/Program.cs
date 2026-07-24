3ec5f7f4059680d8d8d8a18cf6e8c030b9cc34a1
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-24 20:30:17 -0300

    feat: solve exercise 07

---
+++ Exercicio07NumInteiro/Exercicio07NumInteiro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio07NumInteiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Digite um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine("O número é positivo.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("O número é negativo.");
+            }
+            else
+            {
+                Console.WriteLine("O número é zero.");
+            }
+        }
+    }
+}
