43b126290dba79eccf23466ee32f15092557744d
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-26 23:49:36 -0300

    feat: solve exercise 09

---
+++ Exercicio09Multiplos/Exercicio09Multiplos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio09Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+
+            Console.WriteLine("Digite dois números inteiros:");
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
