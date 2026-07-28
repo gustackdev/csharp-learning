8ee312492c8676cc543708aee868d884bbb2a50e
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-27 23:39:12 -0300

    feat: solve exercise 10

---
+++ Exercicio10DuracaoJogo/Exercicio10DuracaoJogo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, duracao;
+
+            Console.Write("Digite a hora inicial: ");
+            horaInicial = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a hora final: ");
+            horaFinal = int.Parse(Console.ReadLine());
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
