49ea4e17ee5768dc05f21440f1984b35637a0ad2
Gustavo S. Andrade <guspatty17@gmail.com>
2026-07-29 19:57:12 -0300

    feat: solve exercise 11

---
+++ Exercicio11Lanchonete/Exercicio11Lanchonete/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio05Lanchonete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo, quantidade;
+            double total = 0.0;
+
+            Console.Write("Digite o código do item: ");
+            codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            if (codigo == 1)
+            {
+                total = quantidade * 4.00;
+            }
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.50;
+            }
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.00;
+            }
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.00;
+            }
+            else if (codigo == 5)
+            {
+                total = quantidade * 1.50;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
