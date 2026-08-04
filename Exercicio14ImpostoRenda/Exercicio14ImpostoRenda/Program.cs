b40f98b10fc3d5cf2fa12c5903418ec694166593
Gustavo S. Andrade <guspatty17@gmail.com>
2026-08-03 23:49:09 -0300

    feat: solve exercise 13

---
+++ Exercicio14ImpostoRenda/Exercicio14ImpostoRenda/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio08_ImpostoRenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double imposto = 0.0;
+
+            if (salario <= 2000.00)
+            {
+                Console.WriteLine("Isento");
+            }
+            else if (salario <= 3000.00)
+            {
+                imposto = (salario - 2000.00) * 0.08;
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else if (salario <= 4500.00)
+            {
+                imposto = 1000.00 * 0.08;
+                imposto += (salario - 3000.00) * 0.18;
+
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                imposto = 1000.00 * 0.08;
+                imposto += 1500.00 * 0.18;
+                imposto += (salario - 4500.00) * 0.28;
+
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
