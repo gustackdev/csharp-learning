adcf5a5475b1d511d36133112409ea913db23772
Gustavo S. Andrade <guspatty17@gmail.com>
2026-08-12 23:24:36 -0300

    feat: solve exercise 16

---
+++ Exercicio16Coordenadas/Exercicio16Coordenadas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] valores;
+
+        while (true)
+        {
+            valores = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            if (x == 0 || y == 0)
+            {
+                break;
+            }
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("primeiro");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("quarto");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("terceiro");
+            }
+            else
+            {
+                Console.WriteLine("segundo");
+            }
+        }
+    }
+}
