9df298c5311ea14ba52c49537a931588428c5517
Gustavo S. Andrade <guspatty17@gmail.com>
2026-08-15 00:12:53 -0300

    feat: solve exercise 17

---
+++ Exercicio17PostoCombustivel/Exercicio17PostoCombustivel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int quantidadeAlcool = 0;
+        int quantidadeGasolina = 0;
+        int quantidadeDiesel = 0;
+
+        int codigoCombustivel = int.Parse(Console.ReadLine());
+
+        while (codigoCombustivel != 4)
+        {
+            if (codigoCombustivel == 1)
+            {
+                quantidadeAlcool++;
+            }
+            else if (codigoCombustivel == 2)
+            {
+                quantidadeGasolina++;
+            }
+            else if (codigoCombustivel == 3)
+            {
+                quantidadeDiesel++;
+            }
+
+            codigoCombustivel = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("MUITO OBRIGADO");
+        Console.WriteLine("Alcool: " + quantidadeAlcool);
+        Console.WriteLine("Gasolina: " + quantidadeGasolina);
+        Console.WriteLine("Diesel: " + quantidadeDiesel);
+    }
+}
