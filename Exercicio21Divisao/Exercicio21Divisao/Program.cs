94741e6adb632bbd6ed73162e600bb8ad04ec0c6
Gustavo S. Andrade <guspatty17@gmail.com>
2026-09-01 22:47:12 -0300

    feat: solve exercise 21

---
+++ Exercicio21Divisao/Exercicio21Divisao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira a quantidade de pares de números: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"\nPar {i}:");
+
+            Console.Write("Insira o primeiro número: ");
+            double primeiro = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o segundo número: ");
+            double segundo = double.Parse(Console.ReadLine());
+
+            if (segundo == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                double resultado = primeiro / segundo;
+                Console.WriteLine($"Resultado da divisão: {resultado:F1}");
+            }
+        }
+
+        Console.WriteLine("\nPressione qualquer tecla para encerrar...");
+        Console.ReadKey();
+    }
+}
