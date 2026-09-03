37b4d0aff34eb505a19b015bf28c03ab82a9f158
Gustavo S. Andrade <guspatty17@gmail.com>
2026-09-02 23:01:47 -0300

    feat: solve exercise 22

---
+++ Exercicio22Fatoracao/Exercicio22Fatoracao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira um número para calcular o fatorial: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int fatorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine($"Fatorial de {n}: {fatorial}");
+
+        Console.WriteLine("\nPressione qualquer tecla para encerrar...");
+        Console.ReadKey();
+    }
+}
