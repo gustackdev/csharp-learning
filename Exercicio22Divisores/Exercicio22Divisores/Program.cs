7fc6d424b5a5316665303ed66d75b2d9b7546dff
Gustavo S. Andrade <guspatty17@gmail.com>
2026-09-03 21:59:27 -0300

    feat: solve exercise 22

---
+++ Exercicio22Divisores/Exercicio22Divisores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira um número inteiro: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nDivisores de {n}:");
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.WriteLine("\nPressione qualquer tecla para encerrar...");
+        Console.ReadKey();
+    }
+}
